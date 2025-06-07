28d024f991c000e3c0bf7d1c68abe7e8ba941846
dbmello75 <dbmello75@gmail.com>
2025-06-07 18:32:29 +0000

    1st commit

---
+++ jobs.php
@@ -0,0 +1,20 @@
+<?php
+// try {
+//     $dbconn3 = pg_connect("host=jobfising-dev.cqberab5aqog.us-east-1.rds.amazonaws.com port=5432 dbname=jobfishing user=jobfishing password=#Daf223#$%&*(iO");
+//     echo 'connected';
+// } catch (\Throwable $th) {
+//     print_r($th->getMessage());
+// }
+phpinfo();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
